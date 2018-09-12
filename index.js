b2e92853f5fe47f4b3d9b8e192742dc180f99a62
Nick Schot <nickschot@gmail.com>
2018-09-12 15:12:39 +0200

    add contentFor hook for ember-power-select

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-model-select'
+  name: 'ember-model-select',
+
+  contentFor(type, config) {
+    let emberPowerSelect = this.addons.find((a) => a.name === 'ember-power-select');
+    return emberPowerSelect.contentFor(type, config);
+  }
 };

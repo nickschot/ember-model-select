0f75cd5c84ae54d51355bfb3fe758c9f196622bb
Nick Schot <nickschot@gmail.com>
2018-11-15 14:22:06 +0100

    add some integration tests remove erronous mirage factory export

--- tests/integration/components/model-select-test.js
+++ tests/integration/components/model-select-test.js
@@ -1,14 +1,59 @@
-import { module, test } from 'qunit';
+import { module } from 'qunit';
+import test from 'ember-sinon-qunit/test-support/test';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { selectChoose } from 'ember-power-select/test-support';
+import { clickTrigger, typeInSearch } from 'ember-power-select/test-support/helpers';
+import defaultScenario from '../../../../dummy/mirage/scenarios/default';
 
 module('Integration | Component | model-select', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
   test('it renders', async function(assert) {
-    await render(hbs`{{model-select modelName='user' labelProperty='name'}}`);
+    defaultScenario(this.server);
 
-    assert.equal(this.element.textContent.trim(), '');
+    await render(hbs`{{model-select modelName='user' labelProperty='name'}}`);
+    await clickTrigger('.ember-model-select');
+
+    assert.dom('.ember-power-select-option').exists({ count: 25 });
+  });
+
+  test('it respects the page size option', async function(assert) {
+    assert.expect(1);
+    defaultScenario(this.server);
+
+    await render(hbs`{{model-select modelName='user' labelProperty='name' pageSize=10}}`);
+    await clickTrigger('.ember-model-select');
+
+    assert.dom('.ember-power-select-option').exists({ count: 10 });
+  });
+
+  test('it limits shown results based on search', async function(assert) {
+    assert.expect(1);
+    defaultScenario(this.server);
+
+    await render(hbs`{{model-select modelName='user' labelProperty='name'}}`);
+    await clickTrigger('.ember-model-select');
+    await typeInSearch('asdasdasd');
+
+    await settled();
+
+    assert.dom('.ember-power-select-option').exists({ count: 1 });
+  });
+
+  test('it triggers the onChange hook when an option is selected', async function(assert) {
+    assert.expect(1);
+    defaultScenario(this.server);
+
+    let handleClick = this.spy();
+    this.actions = { handleClick };
+
+    await render(hbs`{{model-select modelName='user' labelProperty='name' onChange=(action 'handleClick')}}`);
+    await selectChoose('.ember-model-select', '.ember-power-select-option', 1);
+
+    assert.ok(handleClick.calledOnce, 'onChange hook has been called');
   });
 });

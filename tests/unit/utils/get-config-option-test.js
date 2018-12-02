2a9d0ebe26ad044da3fc08c9cb980dd011228211
Nick Schot <nickschot@gmail.com>
2018-12-02 15:14:57 +0100

    add global config support for ember-infinity arguments

--- tests/unit/utils/get-config-option-test.js
+++ tests/unit/utils/get-config-option-test.js
@@ -1,11 +1,13 @@
 import getConfigOption from 'dummy/utils/get-config-option';
 import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
 module('Unit | Utility | get-config-option', function(hooks) {
+  setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it works', function(assert) {
-    let result = getConfigOption();
-    assert.ok(result);
+  test('it returns the passed default when the global option is not set', function(assert) {
+    assert.expect(1);
+
+    assert.equal(getConfigOption('perPageParam', 'foo'), 'foo');
   });
 });

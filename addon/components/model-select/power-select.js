b8977b38286a58577a558f8d78f3886c9dee8f50
Nick Schot <nickschot@gmail.com>
2018-12-06 10:34:48 +0100

    use custom template for power-select so we don't have to explicitly set loading mask height

---
+++ addon/components/model-select/power-select.js
@@ -0,0 +1,6 @@
+import PowerSelect from 'ember-power-select/components/power-select';
+import layout from '../../templates/components/model-select/power-select';
+
+export default PowerSelect.extend({
+  layout
+});

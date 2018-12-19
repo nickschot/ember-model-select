57cf62138a33c14cd906e94d5bae56854bfb92f7
Nick Schot <nickschot@gmail.com>
2018-12-19 15:46:48 +0100

    add multiple item selection test

--- tests/integration/components/model-select-multiple-test.js
+++ tests/integration/components/model-select-multiple-test.js
@@ -4,6 +4,7 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { selectChoose } from 'ember-power-select/test-support';
 import { clickTrigger } from 'ember-power-select/test-support/helpers';
 import defaultScenario from '../../../../dummy/mirage/scenarios/default';
 
@@ -21,4 +22,20 @@ module('Integration | Component | model-select-multiple', function(hooks) {
 
     assert.dom('.ember-power-select-option').exists({ count: 25 });
   });
+
+  test('you can select multiple items', async function(assert) {
+    assert.expect(2);
+
+    defaultScenario(this.server);
+
+    this.set('selected', null);
+
+    await render(hbs`{{model-select-multiple modelName='user' labelProperty='name' selectedModel=selected onchange=(action (mut selected))}}`);
+
+    await selectChoose('.ember-model-select-multiple-trigger', '.ember-power-select-option', 1);
+    await selectChoose('.ember-model-select-multiple-trigger', '.ember-power-select-option', 2);
+
+    assert.equal(this.selected.length, 2, 'two options have been selected');
+    assert.dom('.ember-power-select-multiple-option').exists({ count: 2 });
+  })
 });

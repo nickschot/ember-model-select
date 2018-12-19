3264dd20a9f0ec7c78621420319798b6d443cfd6
Nick Schot <nickschot@gmail.com>
2018-12-19 12:08:11 +0100

    first implementation of a multiple select variant

---
+++ addon/components/model-select-multiple.js
@@ -0,0 +1,6 @@
+import PowerSelectMultipleComponent from 'ember-power-select/components/power-select-multiple';
+import layout from '../templates/components/model-select-multiple';
+
+export default PowerSelectMultipleComponent.extend({
+  layout
+});

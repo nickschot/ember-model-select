9710364fc032e254b8782f15feeb4d07ebb7a877
Nick Schot <nickschot@gmail.com>
2018-12-19 22:13:30 +0100

    add dummy variants for all varieties of model-select add post endpoint for users in mirage

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -3,5 +3,11 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   customQuery: Object.freeze({
     sort: 'name'
-  })
+  }),
+
+  actions: {
+    createUser(name){
+      this.store.createRecord('user', { name }).save();
+    }
+  }
 });

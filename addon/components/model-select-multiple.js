c4a0d46b33c2984ff8c9c8e86071eb3f17c5709a
Nick Schot <nickschot@gmail.com>
2018-12-20 12:22:56 +0100

    add create support to multiple-select

--- addon/components/model-select-multiple.js
+++ addon/components/model-select-multiple.js
@@ -5,5 +5,17 @@ import fallbackIfUndefined from '../utils/computed-fallback-if-undefined';
 export default PowerSelectMultipleComponent.extend({
   layout,
 
-  triggerClass: fallbackIfUndefined('ember-model-select-multiple-trigger')
+  triggerClass: fallbackIfUndefined('ember-model-select-multiple-trigger'),
+
+  actions: {
+    change(option, select){
+      const suggestion = option.find(item => item.__isSuggestion__);
+
+      if(suggestion){
+        this.get('oncreate')(suggestion.__value__, select);
+      } else {
+        this.get('onchange')(option, select);
+      }
+    }
+  }
 });

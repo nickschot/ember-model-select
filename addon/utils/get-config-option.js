c86e4df3f558c1cf1dd0f56fcc8c3ed35e3d2e13
Nick Schot <nickschot@gmail.com>
2018-12-21 16:33:08 +0100

    fix yuidoc for util

--- addon/utils/get-config-option.js
+++ addon/utils/get-config-option.js
@@ -5,9 +5,12 @@ import config from 'ember-get-config';
  *  Get's the passed configuration option from the `ember-model-select`
  *  environment key or the passed default if the config option is not set.
  *
- * @param key
- * @param defaultValue
- * @returns {*}
+ * @function getConfigOption
+ * @param {String} key
+ * @param {*} defaultValue
+ * @return {*} The config option or the default value
+ * @private
+ * @hide
  */
 export default function getConfigOption(key, defaultValue) {
   return getWithDefault(config, `ember-model-select.${key}`, defaultValue);

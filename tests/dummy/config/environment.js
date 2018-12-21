f94cfa4edcdc7e764025ed0625eb15c12fc31adb
Nick Schot <nickschot@gmail.com>
2018-12-21 14:05:39 +0100

    rename dummy -> ember-model-select

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'dummy',
+    modulePrefix: 'ember-model-select',
     environment,
     rootURL: '/',
     locationType: 'auto',
